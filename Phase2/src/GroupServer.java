e90fafe0bcc374a686a30ca06d78397b07e717d6
ALICE <turgus.cassie@gmail.com>
2016-03-17 12:24:51 -0500

    Renamed classes for consistency and added List imports

--- Phase2/src/GroupServer.java
+++ Phase2/src/GroupServer.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class GroupServer
 {
 	List<UserTokenImp> users;
--- Phase2/src/UserTokenImp.java
+++ Phase2/src/UserTokenImp.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class UserTokenImp implements UserToken
 {
 	String tokenIssuer;

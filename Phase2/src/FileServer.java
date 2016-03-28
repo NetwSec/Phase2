733ed221fe9268f412f280461e879b2e3d3783ed
ALICE <turgus.cassie@gmail.com>
2016-03-28 14:02:09 -0500

    Quieted errors in FileServer

--- Phase2/src/FileServer.java
+++ Phase2/src/FileServer.java
@@ -3,13 +3,21 @@ import java.util.List;
 public class FileServer
 {
 	boolean connect(String server, int port)
-	{}
+	{
+            return false;
+        }
 	void disconnect()
 	{}
 	List<String> listFiles(UserToken token) // Move to GroupServer?
-	{}
+	{
+            return null;
+        }
 	boolean upload(String sourceFile, String destFile, String group, UserToken token)
-	{}
+	{
+            return false;
+        }
 	boolean download(String sourceFile, String destFile, UserToken token)
-	{}
+	{
+            return false;
+        }
 }

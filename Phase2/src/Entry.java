064a44196e760b64bedb586f37ef97bce0f0d0fc
牧瀬紅莉栖 <saberconer@gmail.com>
2016-04-01 19:54:40 -0500

    Add fast start

--- Phase2/src/Entry.java
+++ Phase2/src/Entry.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -25,7 +26,7 @@ public class Entry {
             Server.run();
         }
     };
-    
+
     static ClientFramework FileServer = new ClientFramework("Start file server") {
         @Override
         public void run() {
@@ -45,7 +46,7 @@ public class Entry {
             Server.run();
         }
     };
-    
+
     static ClientFramework Client = new ClientFramework("Start client") {
         @Override
         public void run() {
@@ -55,12 +56,35 @@ public class Entry {
     };
 
     static ClientFramework Launcher = new ClientFramework("Launcher");
-    
+
     public static void main(String[] args) {
-        Launcher.RegisterItem(GroupServer);
-        Launcher.RegisterItem(FileServer);
-        Launcher.RegisterItem(Client);
-        Launcher.run();
+        ArrayList<ClientFramework> Subtask = new ArrayList<ClientFramework>();
+        Subtask.add(GroupServer);
+        Subtask.add(FileServer);
+        Subtask.add(Client);
+
+        if (args.length == 0) {
+            Launcher.RegisterItem(Subtask);
+            Launcher.run();
+        } else {
+            try {
+                int TaskID = Integer.parseInt(args[0]);
+                Subtask.get(TaskID).run();
+            } catch (Exception e) {
+                System.out.println("Invalid argument");
+                System.out.println();
+                System.out.println("Usage");
+                try {
+                    System.out.println("java -jar " + Entry.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath() + " <Subtask ID>");
+                } catch (Exception ex) {
+                    System.out.println("java -jar <Path of this file> <Subtask ID>");
+                }
+                System.out.println("Available subtask ID:");
+                for (int i = 0; i < Subtask.size(); i++) {
+                    System.out.println(i + " = " + Subtask.get(i).GetName());
+                }
+            }
+        }
     }
 
 }
---
+++ start.cmd
@@ -0,0 +1,3 @@
+start "Group server" java -jar .\Phase2\dist\Phase2.jar 0
+start "File server" java -jar .\Phase2\dist\Phase2.jar 1
+start "Client" java -jar .\Phase2\dist\Phase2.jar 2
